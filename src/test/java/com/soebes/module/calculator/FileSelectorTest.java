a47d6a70b52e919a9094ed75dd9e3c6a26364c7d
Karl Heinz Marbaise <kama@soebes.de>
2023-08-07 22:13:05 +0200

    * Refactored. * Changed to use JUnit Jupiter extension.

--- src/test/java/com/soebes/module/calculator/FileSelectorTest.java
+++ src/test/java/com/soebes/module/calculator/FileSelectorTest.java
@@ -19,8 +19,9 @@ package com.soebes.module.calculator;
  * under the License.
  */
 
-import com.github.marschall.memoryfilesystem.MemoryFileSystemBuilder;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -37,123 +38,81 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * @author Karl Heinz Marbaise
  */
+@ExtendWith(FileSystemExtension.class)
 class FileSelectorTest {
+
+  private Path root;
+
+  @BeforeEach
+  void beforeEach(FileSystem fileSystem) throws IOException {
+    root = fileSystem.getPath("/");
+    Files.createDirectories(root);
+    Files.createDirectories(root.resolve("/root-project/.git"));
+    Files.createDirectories(root.resolve("/root-project/.github"));
+    Files.createDirectories(root.resolve("/root-project/target"));
+    Files.createDirectories(root.resolve("/root-project/src/main/java/com/example"));
+    Files.createDirectories(root.resolve("/root-project/src/test/java/com/example"));
+    Files.createDirectories(root.resolve("/root-project/target/maven-status"));
+
+    Files.write(root.resolve("/root-project/.git/example-in-git"), "example in .git directory".getBytes(StandardCharsets.UTF_8));
+    Files.write(root.resolve("/root-project/.github/example-in-github"), "example in .github directory".getBytes(StandardCharsets.UTF_8));
+    Files.write(root.resolve("/root-project/target/first.class"), "Example in target".getBytes(StandardCharsets.UTF_8));
+    Files.write(root.resolve("/root-project/src/main/java/com/example/FirstJava.java"), "Das ist ein Test".getBytes(StandardCharsets.UTF_8));
+    Files.write(root.resolve("/root-project/pom.xml"), "pom of the project.".getBytes(StandardCharsets.UTF_8));
+  }
 
   @Test
   void excludesNothing() throws IOException {
-    try (FileSystem fileSystem = MemoryFileSystemBuilder.newLinux().build()) {
-      Path root = fileSystem.getPath("/");
-      Files.createDirectories(root);
-      Files.createDirectories(root.resolve("/root-project/.git"));
-      Files.createDirectories(root.resolve("/root-project/.github"));
-      Files.createDirectories(root.resolve("/root-project/target"));
-      Files.createDirectories(root.resolve("/root-project/src/main/java/com/example"));
-      Files.createDirectories(root.resolve("/root-project/src/test/java/com/example"));
-      Files.createDirectories(root.resolve("/root-project/target/maven-status"));
 
-      Files.write(root.resolve("/root-project/.git/example-in-git"),"example in .git directory".getBytes(StandardCharsets.UTF_8));
-      Files.write(root.resolve("/root-project/.github/example-in-github"),"example in .github directory".getBytes(StandardCharsets.UTF_8));
-      Files.write(root.resolve("/root-project/target/first.class"),"Example in target".getBytes(StandardCharsets.UTF_8));
-      Files.write(root.resolve("/root-project/src/main/java/com/example/FirstJava.java"),"Das ist ein Test".getBytes(StandardCharsets.UTF_8));
-      Files.write(root.resolve("/root-project/pom.xml"), "pom of the project.".getBytes(StandardCharsets.UTF_8));
-      List<Path> result = FileSelector.selectFiles(root.resolve("/root-project"), Collections.emptyList());
+    List<Path> result = FileSelector.selectFiles(root.resolve("/root-project"), Collections.emptyList());
 
-      List<Path> expectedResult = Arrays.asList(
-          root.resolve("/root-project/.git/example-in-git"),
-          root.resolve("/root-project/.github/example-in-github"),
-          root.resolve("/root-project/pom.xml"),
-          root.resolve("/root-project/target/first.class"),
-          root.resolve("/root-project/src/main/java/com/example/FirstJava.java")
-      );
-      assertThat(result).containsExactlyInAnyOrderElementsOf(expectedResult);
-    }
+    List<Path> expectedResult = Arrays.asList(
+        root.resolve("/root-project/.git/example-in-git"),
+        root.resolve("/root-project/.github/example-in-github"),
+        root.resolve("/root-project/pom.xml"),
+        root.resolve("/root-project/target/first.class"),
+        root.resolve("/root-project/src/main/java/com/example/FirstJava.java")
+    );
+    assertThat(result).containsExactlyInAnyOrderElementsOf(expectedResult);
   }
 
   @Test
   void excludesSingleTarget() throws IOException {
-    try (FileSystem fileSystem = MemoryFileSystemBuilder.newLinux().build()) {
-      Path root = fileSystem.getPath("/");
-      Files.createDirectories(root);
-      Files.createDirectories(root.resolve("/root-project/.git"));
-      Files.createDirectories(root.resolve("/root-project/.github"));
-      Files.createDirectories(root.resolve("/root-project/target"));
-      Files.createDirectories(root.resolve("/root-project/src/main/java/com/example"));
-      Files.createDirectories(root.resolve("/root-project/src/test/java/com/example"));
-      Files.createDirectories(root.resolve("/root-project/target/maven-status"));
 
-      Files.write(root.resolve("/root-project/.git/example-in-git"),"example in .git directory".getBytes(StandardCharsets.UTF_8));
-      Files.write(root.resolve("/root-project/.github/example-in-github"),"example in .github directory".getBytes(StandardCharsets.UTF_8));
-      Files.write(root.resolve("/root-project/target/first.class"),"Example in target".getBytes(StandardCharsets.UTF_8));
-      Files.write(root.resolve("/root-project/src/main/java/com/example/FirstJava.java"),"Das ist ein Test".getBytes(StandardCharsets.UTF_8));
-      Files.write(root.resolve("/root-project/pom.xml"), "pom of the project.".getBytes(StandardCharsets.UTF_8));
+    List<Path> result = FileSelector.selectFiles(root.resolve("/root-project"), Collections.singletonList("target"));
 
-      List<Path> result = FileSelector.selectFiles(root.resolve("/root-project"), Collections.singletonList("target"));
-
-      List<Path> expectedResult = Arrays.asList(
-          root.resolve("/root-project/.git/example-in-git"),
-          root.resolve("/root-project/.github/example-in-github"),
-          root.resolve("/root-project/pom.xml"),
-          root.resolve("/root-project/src/main/java/com/example/FirstJava.java")
-      );
-      assertThat(result).containsExactlyInAnyOrderElementsOf(expectedResult);
-    }
+    List<Path> expectedResult = Arrays.asList(
+        root.resolve("/root-project/.git/example-in-git"),
+        root.resolve("/root-project/.github/example-in-github"),
+        root.resolve("/root-project/pom.xml"),
+        root.resolve("/root-project/src/main/java/com/example/FirstJava.java")
+    );
+    assertThat(result).containsExactlyInAnyOrderElementsOf(expectedResult);
   }
 
   @Test
   void excludesTargetAndGit() throws IOException {
-    try (FileSystem fileSystem = MemoryFileSystemBuilder.newLinux().build()) {
-      Path root = fileSystem.getPath("/");
-      Files.createDirectories(root);
-      Files.createDirectories(root.resolve("/root-project/.git"));
-      Files.createDirectories(root.resolve("/root-project/.github"));
-      Files.createDirectories(root.resolve("/root-project/target"));
-      Files.createDirectories(root.resolve("/root-project/src/main/java/com/example"));
-      Files.createDirectories(root.resolve("/root-project/src/test/java/com/example"));
-      Files.createDirectories(root.resolve("/root-project/target/maven-status"));
 
-      Files.write(root.resolve("/root-project/.git/example-in-git"),"example in .git directory".getBytes(StandardCharsets.UTF_8));
-      Files.write(root.resolve("/root-project/.github/example-in-github"),"example in .github directory".getBytes(StandardCharsets.UTF_8));
-      Files.write(root.resolve("/root-project/target/first.class"),"Example in target".getBytes(StandardCharsets.UTF_8));
-      Files.write(root.resolve("/root-project/src/main/java/com/example/FirstJava.java"),"Das ist ein Test".getBytes(StandardCharsets.UTF_8));
-      Files.write(root.resolve("/root-project/pom.xml"), "pom of the project.".getBytes(StandardCharsets.UTF_8));
+    List<Path> result = FileSelector.selectFiles(root.resolve("/root-project"), Arrays.asList("target", ".git"));
 
-      List<Path> result = FileSelector.selectFiles(root.resolve("/root-project"), Arrays.asList("target", ".git"));
-
-      List<Path> expectedResult = Arrays.asList(
-          root.resolve("/root-project/.github/example-in-github"),
-          root.resolve("/root-project/pom.xml"),
-          root.resolve("/root-project/src/main/java/com/example/FirstJava.java")
-      );
-      assertThat(result).containsExactlyInAnyOrderElementsOf(expectedResult);
-    }
+    List<Path> expectedResult = Arrays.asList(
+        root.resolve("/root-project/.github/example-in-github"),
+        root.resolve("/root-project/pom.xml"),
+        root.resolve("/root-project/src/main/java/com/example/FirstJava.java")
+    );
+    assertThat(result).containsExactlyInAnyOrderElementsOf(expectedResult);
   }
 
   @Test
   void excludesTargetGitGitHub() throws IOException {
-    try (FileSystem fileSystem = MemoryFileSystemBuilder.newLinux().build()) {
-      Path root = fileSystem.getPath("/");
-      Files.createDirectories(root);
-      Files.createDirectories(root.resolve("/root-project/.git"));
-      Files.createDirectories(root.resolve("/root-project/.github"));
-      Files.createDirectories(root.resolve("/root-project/target"));
-      Files.createDirectories(root.resolve("/root-project/src/main/java/com/example"));
-      Files.createDirectories(root.resolve("/root-project/src/test/java/com/example"));
-      Files.createDirectories(root.resolve("/root-project/target/maven-status"));
 
-      Files.write(root.resolve("/root-project/.git/example-in-git"),"example in .git directory".getBytes(StandardCharsets.UTF_8));
-      Files.write(root.resolve("/root-project/.github/example-in-github"),"example in .github directory".getBytes(StandardCharsets.UTF_8));
-      Files.write(root.resolve("/root-project/target/first.class"),"Example in target".getBytes(StandardCharsets.UTF_8));
-      Files.write(root.resolve("/root-project/src/main/java/com/example/FirstJava.java"),"Das ist ein Test".getBytes(StandardCharsets.UTF_8));
-      Files.write(root.resolve("/root-project/pom.xml"), "pom of the project.".getBytes(StandardCharsets.UTF_8));
+    List<Path> result = FileSelector.selectFiles(root.resolve("/root-project"), Arrays.asList("target", ".git", ".github"));
 
-      List<Path> result = FileSelector.selectFiles(root.resolve("/root-project"), Arrays.asList("target", ".git", ".github"));
-
-      List<Path> expectedResult = Arrays.asList(
-          root.resolve("/root-project/pom.xml"),
-          root.resolve("/root-project/src/main/java/com/example/FirstJava.java")
-      );
-      assertThat(result).containsExactlyInAnyOrderElementsOf(expectedResult);
-    }
+    List<Path> expectedResult = Arrays.asList(
+        root.resolve("/root-project/pom.xml"),
+        root.resolve("/root-project/src/main/java/com/example/FirstJava.java")
+    );
+    assertThat(result).containsExactlyInAnyOrderElementsOf(expectedResult);
   }
 
 
--- src/test/java/com/soebes/module/calculator/FileSystemExtension.java
+++ src/test/java/com/soebes/module/calculator/FileSystemExtension.java
@@ -4,26 +4,55 @@ import com.github.marschall.memoryfilesystem.MemoryFileSystemBuilder;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.ParameterContext;
+import org.junit.jupiter.api.extension.ParameterResolutionException;
+import org.junit.jupiter.api.extension.ParameterResolver;
 
 import java.nio.file.FileSystem;
 
-class FileSystemExtension implements BeforeEachCallback, AfterEachCallback {
+import static java.util.Objects.nonNull;
 
-  private FileSystem fileSystem;
+class FileSystemExtension implements BeforeEachCallback, AfterEachCallback, ParameterResolver {
 
-  FileSystem getFileSystem() {
-    return this.fileSystem;
+  private static final String FILE_SYSTEM = "file_system";
+
+  private static final ExtensionContext.Namespace FILE_SYSTEM_EXTENSION_NAMESPACE = ExtensionContext.Namespace.create(FileSystemExtension.class.getName());
+
+  private ExtensionContext.Store getContext(ExtensionContext context) {
+    return context.getStore(FILE_SYSTEM_EXTENSION_NAMESPACE);
   }
 
   @Override
   public void beforeEach(ExtensionContext context) throws Exception {
-    this.fileSystem = MemoryFileSystemBuilder.newEmpty().build("name");
+    FileSystem fileSystem = MemoryFileSystemBuilder.newLinux().build(context.getUniqueId());
+    getContext(context).put(FILE_SYSTEM + context.getUniqueId(), fileSystem);
   }
 
   @Override
   public void afterEach(ExtensionContext context) throws Exception {
-    if (this.fileSystem != null) {
-      this.fileSystem.close();
+    FileSystem fileSystem = getContext(context).get(FILE_SYSTEM + context.getUniqueId(), FileSystem.class);
+    if (nonNull(fileSystem)) {
+      fileSystem.close();
     }
   }
+
+  @Override
+  public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    if (!extensionContext.getElement().isPresent()) {
+      return false;
+    }
+
+    Class<?> type = parameterContext.getParameter().getType();
+    return type == FileSystem.class;
+  }
+
+  @Override
+  public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+    Class<?> type = parameterContext.getParameter().getType();
+    if (type == FileSystem.class) {
+      return getContext(extensionContext).get(FILE_SYSTEM + extensionContext.getUniqueId(), FileSystem.class);
+    }
+    return null;
+  }
+
 }
